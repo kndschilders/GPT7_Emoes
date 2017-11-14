c89c2eeee8aea61cba1c9c75b114a9846563fb0e
JimSanders96 <jimsandersgamedev@gmail.com>
2017-11-14 12:08:22 +0100

    Added initial code to showcase use of scriptable object references n stuff

---
+++ GPT_Emoes/Assets/Modular Building Blocks/Variables/Vector3Reference.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace RoboRyanTron.Unite2017.Variables
+{
+    [Serializable]
+    public class Vector3Reference
+    {
+        public bool UseConstant = true;
+        public Vector3 ConstantValue;
+        public Vector3Variable Variable;
+
+        public Vector3Reference()
+        { }
+
+        public Vector3Reference(Vector3 value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public Vector3 Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+
+        public static implicit operator Vector3(Vector3Reference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
---
+++ GPT_Emoes/Assets/Player/Player.cs
@@ -0,0 +1,37 @@
+﻿using RoboRyanTron.Unite2017.Events;
+using RoboRyanTron.Unite2017.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameEvent OnPresenceUpdate;
+    public Vector3Variable LastKnownPos;
+
+    private void Start()
+    {
+        InvokeRepeating("Test", 1f, 1f);
+    }
+
+    private void Test()
+    {
+        transform.Translate(Vector3.one);
+        PresenceUpdate();
+    }
+
+    /// <summary>
+    /// Update last known position and raise the presence update event.
+    /// </summary>
+    private void PresenceUpdate()
+    {
+        UpdateLastKnownPos();
+        OnPresenceUpdate.Raise();
+    }
+
+    private void UpdateLastKnownPos()
+    {
+        LastKnownPos.SetValue(transform.position);
+    }
+}
