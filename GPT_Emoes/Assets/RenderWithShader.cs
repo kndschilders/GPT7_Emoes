24556c4b04b32d7e8da741acd62eefb2bda82d45
nevin123 <nevin-wouters@hotmail.com>
2017-11-15 15:15:11 +0100

    new maze model + fog shader

---
+++ GPT_Emoes/Assets/RenderWithShader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenderWithShader : MonoBehaviour {
+
+    public Material material;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            float fogValue = material.GetFloat("_depth") + 10 * Time.deltaTime;
+
+            material.SetFloat("_depth", fogValue);
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            float fogValue = material.GetFloat("_depth") - 10 * Time.deltaTime;
+
+            material.SetFloat("_depth", fogValue);
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        //material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, material);
+    }
+}
