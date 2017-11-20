6393484945050805e9c67105a5101142e0ee4f22
nevin123 <nevin-wouters@hotmail.com>
2017-11-20 10:54:46 +0100

    Level with assets

---
+++ GPT_Emoes/Assets/playerFollower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class playerFollower : MonoBehaviour {
+
+    public Transform player;
+    private float heightDifference = 0;
+
+    void Start()
+    {
+        heightDifference = transform.position.y - player.position.y;
+    }
+
+	void Update () {
+        transform.position = player.position + Vector3.up * heightDifference;
+	}
+}
