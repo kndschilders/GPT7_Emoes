00a7f238a2ed93180308aafc9b2175e596fb7193
Koen <kndschilders@gmail.com>
2017-11-28 11:33:56 +0100

    Added main menu and game over scene. Added sceneloader to switch to scenes

---
+++ GPT_Emoes/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void OnGoToGameOver() {
+		SceneManager.LoadScene ("game_over");
+	}
+
+	public void OnGoToMainMenu() {
+		SceneManager.LoadScene ("main_menu");
+	}
+
+	public void OnGoToLabyrinth() {
+		SceneManager.LoadScene ("labyrint_pretty");
+	}
+
+	public void OnGoToJacksPlayground() {
+		SceneManager.LoadScene ("JacksPlayground");
+	}
+}
