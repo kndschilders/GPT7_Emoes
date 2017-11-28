bd6982c3ea1c14bb58da0466dcb31ddac67bf55a
nevin123 <nevin-wouters@hotmail.com>
2017-11-28 15:07:04 +0100

    HeartRate UI with public variable

---
+++ GPT_Emoes/Assets/HeartRateVisuals.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartRateVisuals : MonoBehaviour {
+
+    public int HeartRate = 0;
+
+    public Animator AC;
+    public Text text;
+
+    private void Update()
+    {
+        text.text = HeartRate.ToString();
+        AC.SetFloat("multiplier", (HeartRate / 110f));
+    }
+}
