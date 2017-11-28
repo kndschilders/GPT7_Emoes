6d463efeca9491003ce2dbf1c62de45dfcb3c050
Koen <kndschilders@gmail.com>
2017-11-28 12:20:16 +0100

    Added enemy location indicator

---
+++ GPT_Emoes/Assets/Scripts/LocationIndicatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationIndicatorTest : MonoBehaviour {
+
+	public GameObject Player;
+
+	public GameObject Enemy;
+
+	private MouseLook playerMouseLookScript;
+
+	void Start() {
+		playerMouseLookScript = Player.GetComponent<MouseLook> ();
+	}
+
+	void Update () {
+		GUIManager.instance.UpdateEnemyLocationIndicator (Enemy.transform.position, Player.transform.position, playerMouseLookScript.RotationX + 270.0f);
+	}
+}
