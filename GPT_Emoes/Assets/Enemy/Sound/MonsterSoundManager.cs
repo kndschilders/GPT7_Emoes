d1f8ef1978ae6d6d4224784b516d5338a7ce6c4d
JimSanders96 <jimsandersgamedev@gmail.com>
2017-11-29 11:23:23 +0100

    Linked monster indicators to monsters. Added boolreference scriptable object. Started implementing sound cues for monsters

---
+++ GPT_Emoes/Assets/Enemy/Sound/MonsterSoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MonsterSoundManager : MonoBehaviour {
+
+    public AudioClip[] RoamSounds;
+    public AudioClip[] InvestigateSounds;
+    public AudioClip[] ChaseSounds;
+    public AudioClip WalkSoundLoop;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();   
+    }
+
+    public void PlayChaseCue()
+    {
+        audioSource.PlayOneShot(RandomUtil.RandomElement(ChaseSounds));
+    }
+
+    public void PlayInvestigateCue()
+    {
+        audioSource.PlayOneShot(RandomUtil.RandomElement(InvestigateSounds));
+    }
+
+    public void PlayRoamCue()
+    {
+        audioSource.PlayOneShot(RandomUtil.RandomElement(RoamSounds));
+    }
+
+
+}
