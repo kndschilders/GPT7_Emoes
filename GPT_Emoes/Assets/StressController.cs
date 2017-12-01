517af9feeb79103a96d5921d2f01bb56db4e2275
nevin123 <nevin-wouters@hotmail.com>
2017-12-01 13:09:57 +0100

    stress level bestuurbaar met numpad + / -

---
+++ GPT_Emoes/Assets/StressController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class StressController : MonoBehaviour {
+
+    public FloatVariable stress;
+    public FloatVariable bpmLevel;
+
+    private float stressValue;
+
+	void Update () {
+		if(Input.GetKey(KeyCode.KeypadMinus))
+        {
+            stressValue = (stress.Value - .3f * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.KeypadPlus))
+        {
+            stressValue = (stress.Value + .3f * Time.deltaTime);
+        }
+
+        stressValue = Mathf.Clamp(stressValue, 0, 1);
+
+        stress.SetValue(stressValue);
+        bpmLevel.SetValue(60 + 60 * stressValue);
+	}
+}
